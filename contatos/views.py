18d9eb3d2b6cce294573bf96ce40bb75f2d1a050
charlesdev771 <charlesdev0721@protonmail.com>
2022-07-16 06:39:51 -0300

    Pagination and others

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
-contatos = Contato.objects.all()
 def index(request):
+    contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
